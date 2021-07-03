ddf9bfcd1a04b9f9211c5431a2de93993502bb02
Satish Kumar Prajapati <81314007+ninjasatish@users.noreply.github.com>
2021-07-03 10:25:48 +0530

    Create Competitive Programming Template.cpp

---
+++ Competitive Programming Template.cpp
@@ -0,0 +1,79 @@
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define vi vector<int>
+#define nline "\n"
+#define inf 1e18
+#define print cout 
+#define eb emplace_back
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvb vector<vector<bool>>
+#define vc vector<char>
+#define vvc vector<vector<char>>
+#define modulo 1'000'000'007
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define fs first
+#define sc second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.fs); cerr << ","; _print(p.sc); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*--------------------------------------------------------------------------------------------------------------------------------*/
+
+bool descending (int a , int b){
+   return a>b ;
+}
+
+int main (){
+   #ifndef ONLINE_JUDGE
+      freopen("error.txt", "w", stderr);
+   #endif
+
+   fastio();
+ 
+   int test =1 ;
+   cin >> test ;
+   
+   while(test--){
+      
+
+   }
+ 
+    
+}

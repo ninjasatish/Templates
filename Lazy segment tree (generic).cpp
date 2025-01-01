fa569c1715de82be27de0bc7d1c3fcee02d23484
Satish <armyofficersatish@gmail.com>
2025-01-01 11:58:54 +0530

    lazy segtree added

---
+++ Lazy segment tree (generic).cpp
@@ -0,0 +1,94 @@
+
+template<class Info, class Tag>
+struct LazySegmentTree {
+  int n;
+  vector<Info> info;
+  vector<Tag> tag;
+  LazySegmentTree() : n(0) {}
+  LazySegmentTree(int n_, Info v_ = Info()) {
+    init(n_, v_);
+  }
+  template<class T>
+  LazySegmentTree(vector<T> init_) {
+    init(init_);
+  }
+  void init(int n_, Info v_ = Info()) {
+    init(vector<Info>(n_, v_));
+  }
+  template<class T>
+  void init(vector<T> init_) {
+    n = init_.size();
+    info.assign(4 << __lg(n), Info());
+    tag.assign(4 << __lg(n), Tag());
+    function<void(int, int, int)> build = [&](int p, int l, int r) {
+      if (r - l == 1) {
+        info[p] = {init_[l]};
+        return;
+      }
+      int m = (l + r) / 2;
+      build(2 * p, l, m);
+      build(2 * p + 1, m, r);
+      pull(p);
+    };
+    build(1, 0, n);
+  }
+  void pull(int p) {
+    info[p] = info[2 * p] + info[2 * p + 1];
+  }
+  void apply(int p, const Tag &v) {
+    info[p].apply(v);
+    tag[p].apply(v);
+  }
+  void push(int p) {
+    if(tag[p] == Tag()) return;
+    apply(2 * p, tag[p]);
+    apply(2 * p + 1, tag[p]);
+    tag[p] = Tag();
+  }
+  Info rangeQuery(int p, int l, int r, int x, int y) {
+    if (l >= y || r <= x) return Info();
+    if (l >= x && r <= y) return info[p];
+    
+    int m = (l + r) / 2;
+    push(p);
+    return rangeQuery(2 * p, l, m, x, y) + rangeQuery(2 * p + 1, m, r, x, y);
+  }
+  Info rangeQuery(int l, int r) {
+    return rangeQuery(1, 0, n, l, r);
+  }
+  void rangeApply(int p, int l, int r, int x, int y, const Tag &v) {
+    if (l >= y || r <= x) return;
+    if (l >= x && r <= y) {
+      apply(p, v);
+      return;
+    }
+    int m = (l + r) / 2;
+    push(p);
+    rangeApply(2 * p, l, m, x, y, v);
+    rangeApply(2 * p + 1, m, r, x, y, v);
+    pull(p);
+  }
+  void rangeApply(int l, int r, const Tag &v) {
+    return rangeApply(1, 0, n, l, r, v);
+  }
+  template<class F>
+  int findFirst(int p, int l, int r, int x, int y, F &&pred) {
+    if (l >= y || r <= x) return -1;
+    if (l >= x && r <= y && !pred(info[p])) return -1;
+    if (r - l == 1) return l;
+    
+    int m = (l + r) / 2;
+    push(p);
+    int res = findFirst(2 * p, l, m, x, y, pred);
+    if (res == -1) {
+      res = findFirst(2 * p + 1, m, r, x, y, pred);
+    }
+    return res;
+  }
+  template<class F>
+  int findFirst(int l, int r, F &&pred) {
+    return findFirst(1, 0, n, l, r, pred);
+  }
+};
+
+
